0bfb8fd20d5c38ff9cd710fd6829a682b9fdd7d4
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-09-05 10:31:50 +0300

    i

--- result.php
+++ result.php
@@ -1,5 +1,5 @@
 <?php
-    move_uploaded_file($_FILES['file']['tmp_name'], 'NucleicNet/protein_RNA_interaction_package/GridData');
+    move_uploaded_file($_FILES['file']['tmp_name'], 'NucleicNet/protein_RNA_interaction_package/GridData/1.pdb');
     putenv("PATH=/usr/local/cuda/bin:/usr/local/cuda-8.0/bin:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:");
     putenv("LD_LIBRARY_PATH=/usr/local/cuda/lib64");
     $data = shell_exec ( 'NucleicNet/protein_RNA_interaction_package/commandNAGenerateBlindGrid.sh && NucleicNet/protein_RNA_interaction_package/dl_prediction.sh && NucleicNet/protein_RNA_interaction_package/commandNAAnalyseGridPrediction.sh ' . " 2>&1" );
