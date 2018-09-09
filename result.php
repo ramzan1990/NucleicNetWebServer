e6d421f5d429a9a71ad40d1ba6a99445f071fedb
ramzan1990 <umarov256@gmail.com>
2018-09-09 11:45:05 +0300

    i

--- result.php
+++ result.php
@@ -1,5 +1,17 @@
 <?php
-    move_uploaded_file($_FILES['file']['tmp_name'], 'NucleicNet/protein_RNA_interaction_package/GridData/' . time() . '.pdb');
+	function EmptyDir($dir) {
+	$handle=opendir($dir);
+	while (($file = readdir($handle))!==false) {
+	unlink($dir.'/'.$file);
+	}
+	closedir($handle);
+	}
+
+	EmptyDir('NucleicNet/protein_RNA_interaction_package/GridData');
+	EmptyDir('NucleicNet/protein_RNA_interaction_package/Out');
+	EmptyDir('NucleicNet/protein_RNA_interaction_package/DL_output');
+	
+    move_uploaded_file($_FILES['file']['tmp_name'], 'NucleicNet/protein_RNA_interaction_package/GridData/1111.pdb');
     putenv("PATH=/usr/local/cuda/bin:/usr/local/cuda-8.0/bin:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/conda/bin:/var/www/html/NucleicNet/utils/dssp");
     putenv("LD_LIBRARY_PATH=/usr/local/cuda/lib64");
 
@@ -13,17 +25,17 @@
 	echo '-----------------------------------------------------------------------------------------------------------------<br />';
 	echo nl2br($data);
     echo '-----------------------------------------------------------------------------------------------------------------<br />';
-    echo 'Out<br />'
+    echo 'Out<br />';
     $dir    = 'NucleicNet/protein_RNA_interaction_package/Out';
 	$files1 = scandir($dir);
 	print_r($files1);
     echo '-----------------------------------------------------------------------------------------------------------------<br />';
-    echo 'GridData<br />'
+    echo 'GridData<br />';
 	$dir    = 'NucleicNet/protein_RNA_interaction_package/GridData';
 	$files1 = scandir($dir);
 	print_r($files1);
 	echo '-----------------------------------------------------------------------------------------------------------------<br />';
-	echo 'DL_output<br />'
+	echo 'DL_output<br />';
 	$dir    = 'NucleicNet/protein_RNA_interaction_package/DL_output';
 	$files1 = scandir($dir);
 	print_r($files1);
