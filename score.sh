7ece88488cf42601c8cce0da60de652488a02389
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-10-01 00:44:52 +0300

    i

--- score.sh
+++ score.sh
@@ -1,2 +1,34 @@
+#=============================================================
+
+# Downstream II: Analyse Deep learning output from NucleicNet 
+
+#=============================================================
+
+#__author__ = "Jordy Homing Lam"
+#__copyright__ = "Copyright 2018, Hong Kong University of Science and Technology"
+#__license__ = "3-clause BSD"
+
+
+SCRIPT_HOME='Scripts'                          # Hold Python scripts
+GRAND_DATAFRAME_NA='DataframeGrandProNuc.pvar' # Hold Derived info from Pdb
+Prediction_DIR='DL_output'        # Hold reformat pickled deep learning grid results
+RNA_Pdb_Database_DIR='Pdb'   # Hold Source RNA conformer pdb
+RNA_Mer_Database_DIR='Mer'   # Hold Analysed Mer for RNA pdb
+Docking_Target_Pdb_DIR='Apo' # Hold Target Apo protein 
+Pharm_Output_DIR='Out'       # Hold Clique analysis result
+ICP_Output_DIR='Icp'         # Hold Pdb produce from ICP
+BC_DIR='bc-100.out'          # Hold External redundancy record
+
+PDB_Depository='/media/jhmlam/CAESAR/MessLab/Database-PDB/unzipped/'
+BGSU_DIR='/media/jhmlam/CAESAR/MessLab/Database-Nucleic3Dstructure/BGSU'
+Pyle_DIR='/media/jhmlam/CAESAR/MessLab/Database-Nucleic3Dstructure/PyleRNAlibraries'
+KnownSite_DIR='Control'
+#RNAC_DIR='/home/jhmlam/MessLab/Project-NucleicBind/protein_RNA_interaction_package/entiredata_2018_08_27_5-05_am/'
+RNAC_DIR='/home/jhmlam/MessLab/Project-NucleicBind/protein_RNA_interaction_package/RNACompete_data/'
+
+Working_DIR='GridData'
+
+
 source activate py3
-python Scripts/Nucleic-Bind_HMM_ScoreSomeSequences.py --PredictionFolder Apo --TestSequences $1 --Targetname Apo
+
+python ${SCRIPT_HOME}/Nucleic-Bind_HMM_ScoreSomeSequences.py --PredictionFolder ${Prediction_DIR} --TestSequences $1 --Targetname ${Docking_Target_Pdb_DIR}
