495af43b40a66d71e6efdfc998274d58ed4f58aa
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-10-01 02:42:15 +0300

    i

--- score.sh
+++ score.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source activate py3
-python ${SCRIPT_HOME}/Nucleic-Bind_HMM_ScoreSomeSequences.py --PredictionFolder DL_output --TestSequences $1 --Targetname 4f3t
+python Scripts/Nucleic-Bind_HMM_ScoreSomeSequences.py --PredictionFolder DL_output --TestSequences $1 --Targetname 4f3t
