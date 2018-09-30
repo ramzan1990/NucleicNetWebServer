022e80b223fd9de1dbb3bf10caa17e81e1c830c3
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-10-01 01:48:48 +0300

    i

--- result.php
+++ result.php
@@ -15,7 +15,7 @@
     putenv("PATH=/usr/local/cuda/bin:/usr/local/cuda-8.0/bin:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/conda/bin:/var/www/html/NucleicNet/utils/dssp");
     putenv("LD_LIBRARY_PATH=/usr/local/cuda/lib64");
 
-    $data = shell_exec ( 'cd NucleicNet/protein_RNA_interaction_package/; bash commandNAGenerateBlindGrid.sh' . " 2>&1");
+    $data = shell_exec ( 'cd NucleicNet/protein_RNA_interaction_package/; whoami; bash commandNAGenerateBlindGrid.sh' . " 2>&1");
     echo '-----------------------------------------------------------------------------------------------------------------<br />';
     echo nl2br($data);
 	$data = shell_exec ( 'cd NucleicNet/protein_RNA_interaction_package/; bash dl_prediction.sh' . " 2>&1");
