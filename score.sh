72b11566331472cb801d6b2edd044d0990984801
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-09-30 23:43:25 +0300

    i

--- score.sh
+++ score.sh
@@ -1,2 +1,2 @@
 source activate py3
-python NucleicNet/protein_RNA_interaction_package/Scripts/Nucleic-Bind_HMM_ScoreSomeSequences.py --PredictionFolder Apo --TestSequences $1 --Targetname Apo
+python Scripts/Nucleic-Bind_HMM_ScoreSomeSequences.py --PredictionFolder Apo --TestSequences $1 --Targetname Apo
