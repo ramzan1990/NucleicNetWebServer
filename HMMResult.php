e5cb14f4d5f2d5d80bcf40f930c171939a87047d
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-10-01 02:55:29 +0300

    i

--- HMMResult.php
+++ HMMResult.php
@@ -1,10 +1,7 @@
 <?php
   putenv("PATH=/usr/local/cuda/bin:/usr/local/cuda-8.0/bin:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/conda/bin:/var/www/html/NucleicNet/utils/dssp");
   putenv("LD_LIBRARY_PATH=/usr/local/cuda/lib64");
-  $query_name = "single_" . time();
-  $new_file = "files/" . $query_name .'.csv';
   $content = $_POST["sequence"];
-  file_put_contents($new_file, $content);
-  $data = shell_exec ( 'cd NucleicNet/protein_RNA_interaction_package/; whoami; bash score.sh '. realpath($new_file) . ' 2>&1');
+  $data = shell_exec ( 'cd NucleicNet/protein_RNA_interaction_package/; whoami; bash score.sh '. $content . ' 2>&1');
   echo $data;
 ?>
