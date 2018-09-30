6ad59af9fd11c201bd729d24d55a400b4d604d70
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-09-30 23:22:41 +0300

    i

--- HMMResult.php
+++ HMMResult.php
@@ -1,4 +1,6 @@
 <?php
+  putenv("PATH=/usr/local/cuda/bin:/usr/local/cuda-8.0/bin:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/conda/bin:/var/www/html/NucleicNet/utils/dssp");
+  putenv("LD_LIBRARY_PATH=/usr/local/cuda/lib64");
   $query_name = "single_" . time();
   $new_file = "files/" . $query_name .'.csv';
   $content = $_POST["sequence"];
