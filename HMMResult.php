a7369fa5c65215df103dfa90aaf7735c324dc916
Ramzan Umarov <umarovr@kw12508.cbrc.kaust.edu.sa>
2018-10-01 03:08:19 +0300

    i

--- HMMResult.php
+++ HMMResult.php
@@ -2,6 +2,15 @@
   putenv("PATH=/usr/local/cuda/bin:/usr/local/cuda-8.0/bin:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/conda/bin:/var/www/html/NucleicNet/utils/dssp");
   putenv("LD_LIBRARY_PATH=/usr/local/cuda/lib64");
   $content = $_POST["sequence"];
-  $data = shell_exec ( 'cd NucleicNet/protein_RNA_interaction_package/; whoami; bash score.sh '. $content . ' 2>&1');
-  echo $data;
+  $seqs = explode(',', $content);  
+  $data = shell_exec ( 'cd NucleicNet/protein_RNA_interaction_package/; bash score.sh '. $content);
+  $scores = explode(PHP_EOL, $data);
+  echo "<table><tr><th>Sequence</th><th>Score</th></tr>";
+  for ($x = 0; $x < count($seqs); $x++) {
+      echo "<tr>";
+      echo "<td>" . $seqs[$x] . "</td>";
+      echo "<td>" . $scores[$x] . "</td>";
+      echo "</tr>";
+  }
+  echo "</table>";
 ?>
